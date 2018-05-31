a0f2657badab36d790f58d1be495b5043355532f
whatwewant <tobewhatwewant@gmail.com>
2018-05-31 11:24:41 +0800

    :sparkles: (version 0.0.3)

--- src/components.js
+++ src/components.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 
 const PRELOADNUM = 3;
 
-export class CenterImage extends Component {
+export class CenterImage extends PureComponent {
     state = {
         loading: true,
         error: false,
---
+++ src/libs/singleton.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+
+export default class Singleton {
+  constructor(component) {
+    this.dom = null;
+    this.component = component;
+    this.instance = null;
+  }
+
+  show(option) {
+    if (!this.dom) {
+      this.dom = document.createElement("div");
+      document.body.appendChild(this.dom);
+    }
+    const { component: Component } = this;
+    this.instance = render(<Component {...option} />, this.dom);
+    this.instance.setState({
+      show: true
+    });
+  }
+
+  hide() {
+    if (this.instance) {
+      this.instance.setState(
+        {
+          show: false
+        },
+        () => {
+          setTimeout(() => {
+            unmountComponentAtNode(this.dom);
+          }, 100);
+        }
+      );
+    }
+  }
+}
